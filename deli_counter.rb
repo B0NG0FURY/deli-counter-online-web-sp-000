7b97a630d213a789ab09abbd5d1f85a19956e203
John Troutman <jtroutmanman@gmail.com>
2020-09-18 03:57:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,10 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    line_string = "The line is currently: "
+    line_string = "The line is currently:"
     array.each_with_index do |person, index|
       index += 1
-      line_string += "#{index}. #{person} "
+      line_string += " #{index}. #{person}"
     end
     puts line_string
   end
